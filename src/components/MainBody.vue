bb9bc4e2cdd04d47ddcde1dc6a44886d91e4b550
Jesus Sanchez <tsunatsu.jesg@gmail.com>
2024-12-02 12:16:53 -0700

    cuarto p2 commit

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="MainBody">
     <div class="container-fluid">
-      <div class="mt-10" style="glyph-orientation-vertical: auto;">
+      <div style="glyph-orientation-vertical: auto;">
         <img src="../assets/logo-coppel-salud.svg" alt="Logo-Salud">
         <h2>Tu salud y la de tu familia es lo mas importante</h2>
         <button type="button" class="btn btn-primary">Ir a la Farmacia Digital</button>
