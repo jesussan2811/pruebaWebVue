c822ba61083c8a353bebe564ae14ed0e3d1a9f1b
Jesus Sanchez <tsunatsu.jesg@gmail.com>
2024-12-02 23:49:31 -0700

    quinto commit

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="MainBody">
+  <div class="ms-5 me-5">
     <div class="container-fluid">
-      <div style="glyph-orientation-vertical: auto;">
-        <img src="../assets/logo-coppel-salud.svg" alt="Logo-Salud">
-        <h2>Tu salud y la de tu familia es lo mas importante</h2>
-        <button type="button" class="btn btn-primary">Ir a la Farmacia Digital</button>
+      <div class="row align-items-center">
+        <div class="col-md-6 ">
+          <img src="../assets/logo-coppel-salud.svg" alt="Logo-Salud" class="img-fluid">
+          <h3>Tu salud y la de tu familia es lo más importante</h3>
+          <button type="button" class="btn btn-primary">Ir a la Farmacia Digital</button>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/coppel-salud-vector.svg" alt="Vector-Salud" class="img-fluid">
+        </div>
       </div>
     </div>
+    <div class="container-fluid">
+      <div class="row align-items-center">
+        <div class="col-md-6">
+          <img src="../assets/coppel-salud-familia.png" alt="salud-familia" class="img-fluid">
+        </div>
+        <div class="col-md-6">
+          <h4>¿Que es Coppel Salud?</h4>
+          <p>Es la opcion y accesible para que tu y tu familia reciban los productos y servcios de salud que necesitan, en cualquier momento, desde cualquier lugar y a un precio justo que puedas pagar con tu Credito Coppel</p>
+        </div>
+      </div>
+    </div>
+    <h4>Descubre nuestros servicios</h4>
+    <p>Coppel Salud es un programa integral diseñado</p>    
   </div>
 </template>
 
